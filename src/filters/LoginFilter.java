5157fc93ce132170a1de3bce93632e2c017656b6
miyu <lv_askk_pato_kg@outlook.jp>
2020-05-06 22:39:24 +0900

    Add login /logout link

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -53,7 +53,7 @@ public class LoginFilter implements Filter {
 		            return;
 		        }
 		        
-		        if(servlet_path.matches("/empolyees.*") && e.getAdmin_flag() == 0) {
+		        if(servlet_path.matches("/employees.*") && e.getAdmin_flag() == 0) {
 		            ((HttpServletResponse)response).sendRedirect(context_path + "/");
 		            return;
 		        }
