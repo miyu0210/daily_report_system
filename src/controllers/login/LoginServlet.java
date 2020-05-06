a474d7cf06185be616febe8896d5455c6ac3111f
miyu <lv_askk_pato_kg@outlook.jp>
2020-05-06 21:07:52 +0900

    Add LoginFilter

--- src/controllers/login/LoginServlet.java
+++ src/controllers/login/LoginServlet.java
@@ -84,7 +84,7 @@ public class LoginServlet extends HttpServlet {
 		    request.setAttribute("hasError", true);
 		    request.setAttribute("code", code);
 		    
-		    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/llogin.jsp");
+		    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
 		    rd.forward(request, response);
 		} else {
 		    request.getSession().setAttribute("login_employee", e);
